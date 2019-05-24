4f56d22f4cf4cc46160aea0e050ad466a18a327d
Becky Phan <beckyphan@live.com>
2019-05-24 22:26:30 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -20,8 +20,12 @@ def first_element_with_array_methods(array)
   array.first
 end
 
-def last_element_with_array_methods(array)
+def last_element(array)
   array[-1]
+end
+
+def last_element_with_array_methods(array)
+  array.last
 end
 
 def length_of_array(array)
